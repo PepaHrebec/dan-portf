607cc7fca513d6e0fbbdfbd37c4c97ab64b1ac5d
Josef Hrebec <josefhrebec02@gmail.com>
2023-09-29 18:19:58 +0200

    Add Contentful API

--- src/script.ts
+++ src/script.ts
@@ -1,12 +1,12 @@
 const images = document.querySelectorAll("img");
-const foot = document.querySelector(".foot");
-const footWrap = document.querySelector(".foot-wrap");
+const full = document.querySelector(".full");
+const fullWrap = document.querySelector(".full-wrap");
 images.forEach((image) => {
   image.addEventListener("click", (e) => {
-    (foot as HTMLImageElement).src = (e.target as HTMLInputElement).src;
-    footWrap?.classList.add("displ");
+    (full as HTMLImageElement).src = (e.target as HTMLInputElement).src;
+    fullWrap?.classList.add("displ");
   });
 });
-footWrap?.addEventListener("click", () => {
-  footWrap.classList.remove("displ");
+fullWrap?.addEventListener("click", () => {
+  fullWrap.classList.remove("displ");
 });
