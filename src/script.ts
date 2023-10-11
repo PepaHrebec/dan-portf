1890baa2d2257d35cc9a15b9467b965392ab492a
Josef Hrebec <josefhrebec02@gmail.com>
2023-10-11 14:51:38 +0200

    Add close btn to fullscreen

--- src/script.ts
+++ src/script.ts
@@ -1,12 +1,13 @@
 const images = document.querySelectorAll("img");
 const full = document.querySelector(".full");
 const fullWrap = document.querySelector(".full-wrap");
+const closeBtn = document.querySelector(".close");
 images.forEach((image) => {
   image.addEventListener("click", (e) => {
     (full as HTMLImageElement).src = (e.target as HTMLInputElement).src;
     fullWrap?.classList.add("displ");
   });
 });
-fullWrap?.addEventListener("click", () => {
-  fullWrap.classList.remove("displ");
+closeBtn?.addEventListener("click", () => {
+  fullWrap?.classList.remove("displ");
 });
