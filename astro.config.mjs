981fbaeb33873326edaad297aaf2f51c9e176b86
Josef Hrebec <josefhrebec02@gmail.com>
2023-10-31 19:35:39 +0100

    Add sitemap extension

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   image: {
     domains: ["images.ctfassets.net"],
   },
+  site: "https://stargazers.club",
+  integrations: [sitemap()],
 });
