e26ca77935974544882b2bfe81648d90194f53b5
Josef Hrebec <josefhrebec02@gmail.com>
2023-11-02 13:20:08 +0100

    Add Robots.txt and Sitemap integr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   image: {
     domains: ["images.ctfassets.net"],
   },
-  site: "https://stargazers.club",
-  integrations: [sitemap()],
+  site: "http://kralfoto.cz/",
+  integrations: [sitemap(), robotsTxt()],
 });
